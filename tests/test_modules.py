01f3ddda0b5adc8c72b33ab02190b0316679c0b2
Ilkka Tuohela <hile@iki.fi>
2022-07-26 14:00:19 +0300

    Add utility class to check if all required modules exist

---
+++ tests/test_modules.py
@@ -0,0 +1,29 @@
+"""
+Unit tests for sys_toolkit.modules module
+"""
+
+from sys_toolkit.modules import check_available_imports
+
+MISSING_MODULES_LIST = (
+    'pathlib',
+    'ljjsjfdbjdibifbdibfoboadfboa',
+    'shutil',
+)
+AVAILABLE_MODULES_LIST = (
+    'pathlib',
+    'shutil',
+)
+
+
+def test_modules_check_available_imports_not_found():
+    """
+    Test checking for available import s with random, not found modules
+    """
+    assert check_available_imports(*MISSING_MODULES_LIST) is False
+
+
+def test_modules_check_available_imports_found():
+    """
+    Test checking for available import s with random, not found modules
+    """
+    assert check_available_imports(*AVAILABLE_MODULES_LIST) is True
