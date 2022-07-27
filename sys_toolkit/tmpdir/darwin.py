afbf42b153d4cba63b7f63afc2766185b2e0f864
Ilkka Tuohela <hile@iki.fi>
2022-07-27 11:14:57 +0300

    Add unit tests for the new darwin secure temporary directory context manager

--- sys_toolkit/tmpdir/darwin.py
+++ sys_toolkit/tmpdir/darwin.py
@@ -39,6 +39,8 @@ class DarwinSecureTemporaryDirectory(SecureTemporaryDirectoryBaseClass):
         try:
             command = ('hdid', '-drivekey', 'system-image=yes', '-nomount', f'ram://{self.size}')
             res = run(*command, stdout=PIPE, stderr=PIPE)
+            if res.returncode != 0:
+                raise CommandError(res.stderr)
             self.__device__ = Path(str(res.stdout, encoding='utf-8').rstrip())
         except CommandError as error:
             raise SecureTemporaryDirectoryError(f'Error creating ramdisk: {error}') from error
@@ -46,7 +48,9 @@ class DarwinSecureTemporaryDirectory(SecureTemporaryDirectoryBaseClass):
         self.__check_ramdisk_device__()
         try:
             command = ('newfs_hfs', '-M', '700', self.__device__)
-            run(*command, stdout=PIPE, stderr=PIPE)
+            res = run(*command, stdout=PIPE, stderr=PIPE)
+            if res.returncode != 0:
+                raise CommandError(res.stderr)
         except CommandError as error:
             raise SecureTemporaryDirectoryError(
                 'Error creating filesystem on ramdisk device {self.__device__}: {error'
@@ -68,7 +72,9 @@ class DarwinSecureTemporaryDirectory(SecureTemporaryDirectoryBaseClass):
                 str(self.__device__),
                 str(self.path),
             )
-            run(*command)
+            res = run(*command)
+            if res.returncode != 0:
+                raise CommandError(res.stderr)
         except CommandError as error:
             raise SecureTemporaryDirectoryError(
                 f'Error mounting {self.__device__} to {self.path}: {error}'
@@ -81,7 +87,9 @@ class DarwinSecureTemporaryDirectory(SecureTemporaryDirectoryBaseClass):
         self.__check_ramdisk_device__()
         command = ('diskutil', 'quiet', 'eject', str(self.__device__))
         try:
-            run(*command)
+            res = run(*command)
+            if res.returncode != 0:
+                raise CommandError(res.stderr)
             self.__device__ = None
         except CommandError as error:
             raise SecureTemporaryDirectoryError(
--- tests/tmpdir/test_darwin.py
+++ tests/tmpdir/test_darwin.py
@@ -1,13 +1,18 @@
 """
 Unit tests for sys_toolkit.tmpdir.darwin module
 """
+from pathlib import Path
+
 import pytest
 
 from sys_toolkit.exceptions import SecureTemporaryDirectoryError
 from sys_toolkit.tmpdir.darwin import DarwinSecureTemporaryDirectory
 
+from .conftest import MOCK_DEVICE
 
-def test_tmpdir_darwin_properties():
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_properties(mock_tmpdir_darwin_success):
     """
     Test properties of a darwin secure temporary directory object that has not been initialized
     as a context manager
@@ -19,3 +24,97 @@ def test_tmpdir_darwin_properties():
 
     with pytest.raises(SecureTemporaryDirectoryError):
         tmpdir.__check_ramdisk_device__()
+
+
+def test_tmpdir_darwin_missing_ramdisk_device():
+    """
+    Test darwin temporary directory object check for ramdisk device when device is defined but missing
+    """
+    obj = DarwinSecureTemporaryDirectory()
+    obj.__device__ = Path(MOCK_DEVICE)
+    with pytest.raises(SecureTemporaryDirectoryError):
+        obj.__check_ramdisk_device__()
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_attach_storage_volume_uninitialized(mock_tmpdir_darwin_success):
+    """
+    Ensure attaching storage volume to uninitialized temporary volume fails early
+    """
+    with pytest.raises(SecureTemporaryDirectoryError):
+        DarwinSecureTemporaryDirectory().attach_storage_volume()
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_detach_storage_volume_uninitialized(mock_tmpdir_darwin_success):
+    """
+    Ensure detaching storage volume from uninitialized temporary volume fails early
+    """
+    with pytest.raises(SecureTemporaryDirectoryError):
+        DarwinSecureTemporaryDirectory().detach_storage_volume()
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_create_storage_volume(mock_tmpdir_darwin_success):
+    """
+    Test running create_storage_volume method directly for uninitialized darwin volumes
+    """
+    obj = DarwinSecureTemporaryDirectory()
+    obj.create_storage_volume()
+    assert obj.path is None
+    assert obj.__device__ == Path(MOCK_DEVICE)
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_context_manager_create_error(mock_tmpdir_darwin_create_fail):
+    """
+    Test initializing a darwin secure temporary directory when ramdisk creation fails
+    """
+    with pytest.raises(SecureTemporaryDirectoryError):
+        with DarwinSecureTemporaryDirectory() as obj:
+            obj  # pylint: disable=pointless-statement
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_context_manager_newfs_error(mock_tmpdir_darwin_newfs_fail):
+    """
+    Test initializing a darwin secure temporary directory when filesystem creation fails
+    """
+    with pytest.raises(SecureTemporaryDirectoryError):
+        with DarwinSecureTemporaryDirectory() as obj:
+            obj  # pylint: disable=pointless-statement
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_context_manager_mount_error(mock_tmpdir_darwin_mount_fail):
+    """
+    Test initializing a darwin secure temporary directory when filesystem mounting fails
+    """
+    with pytest.raises(SecureTemporaryDirectoryError):
+        with DarwinSecureTemporaryDirectory() as obj:
+            obj  # pylint: disable=pointless-statement
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_context_manager_detach_error(mock_tmpdir_darwin_diskutil_fail):
+    """
+    Test initializing a darwin secure temporary directory when filesystem detaching fails
+    """
+    with pytest.raises(SecureTemporaryDirectoryError):
+        with DarwinSecureTemporaryDirectory() as obj:
+            obj  # pylint: disable=pointless-statement
+
+
+# pylint: disable=unused-argument
+def test_tmpdir_darwin_context_manager_ok(mock_tmpdir_darwin_success):
+    """
+    Test initializing a darwin secure temporary directory context manager successfully
+    with mocked environment
+
+    All CLI commands are mocked away
+    """
+    with DarwinSecureTemporaryDirectory() as obj:
+        for attr in ('path', '__device__'):
+            value = getattr(obj, attr)
+            assert isinstance(value, Path)
+        assert obj.path.is_dir()
